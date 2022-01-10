09513179d859407f143094931225dbe71736d760
ialexander28 <buildingbusinessideas@gmail.com>
2022-01-10 03:12:04 -0700

    Simplified Code

--- checksum_function.cpp
+++ checksum_function.cpp
@@ -15,20 +15,14 @@ void isequal(double value){
     std::cout << "Enter a second value for v2 : " << '\n';
     std::cin >> v2;
 
-    if(v1 == v2)
-    value = true; 
 
-
-        if(value == true)
+        if(v1 == v2)
         std::cout << " The values are equal " << '\n'; 
 
         else 
         std::cout << " Values are not equal " << '\n'; 
 
         std::cout << '\n'; 
-
-  
-
     
 }
 
@@ -39,6 +33,8 @@ int main()
     double value; 
 
     isequal(value); 
+
+    std::cout << '\n'; 
 
     return 0; 
 }
